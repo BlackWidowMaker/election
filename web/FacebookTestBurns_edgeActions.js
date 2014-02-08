bbc330a2c0087fd6405f3cd7a39ec2e57eefda0f
blackwidowmaker <jtptillman@gmail.com>
2014-02-08 17:13:49 -0600

    fb page 1.0
    good stuff

---
+++ web/FacebookTestBurns_edgeActions.js
@@ -0,0 +1,110 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_TextCopy2}","click",function(sym,e){sym.play("Point1");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2697,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2191,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3193,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3696,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4195,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_But1}","click",function(sym,e){sym.play("Point2");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_But2}","click",function(sym,e){sym.play("Point3");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_But3}","click",function(sym,e){sym.play("Point4");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_But4}","click",function(sym,e){sym.play("Point5");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1500,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'LikeButton'
+(function(symbolName){})("LikeButton");
+//Edge symbol end:'LikeButton'
+
+//=========================================================
+
+//Edge symbol: 'Point1'
+(function(symbolName){})("Point1");
+//Edge symbol end:'Point1'
+
+//=========================================================
+
+//Edge symbol: 'Banner'
+(function(symbolName){})("Banner");
+//Edge symbol end:'Banner'
+
+//=========================================================
+
+//Edge symbol: 'Point1_1'
+(function(symbolName){})("Point2");
+//Edge symbol end:'Point2'
+
+//=========================================================
+
+//Edge symbol: 'Point2_1'
+(function(symbolName){})("Point3");
+//Edge symbol end:'Point3'
+
+//=========================================================
+
+//Edge symbol: 'Point3_1'
+(function(symbolName){})("Point4");
+//Edge symbol end:'Point4'
+
+//=========================================================
+
+//Edge symbol: 'Point3_2'
+(function(symbolName){})("Point5");
+//Edge symbol end:'Point5'
+
+//=========================================================
+
+//Edge symbol: 'But1'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${__1stBut}","click",function(sym,e){sym.play("Point2");});
+//Edge binding end
+})("But1");
+//Edge symbol end:'But1'
+
+//=========================================================
+
+//Edge symbol: 'But1_1'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${__1stBut}","click",function(sym,e){sym.play("Point2");});
+//Edge binding end
+})("But2");
+//Edge symbol end:'But2'
+
+//=========================================================
+
+//Edge symbol: 'But1_2'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${__1stBut}","click",function(sym,e){sym.play("Point2");});
+//Edge binding end
+})("But3");
+//Edge symbol end:'But3'
+
+//=========================================================
+
+//Edge symbol: 'But1_3'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${__1stBut}","click",function(sym,e){sym.play("Point2");});
+//Edge binding end
+})("But4");
+//Edge symbol end:'But4'
+
+//=========================================================
+
+//Edge symbol: 'But1_4'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${__1stBut}","click",function(sym,e){sym.play("Point2");});
+//Edge binding end
+})("But5");
+//Edge symbol end:'But5'
+})(jQuery,AdobeEdge,"EDGE-279288412");
